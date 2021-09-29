281d135d2ccd687f09f22c7559a5db1f2cb8a815
Tim Flood <tflood9@gmail.com>
2021-09-29 17:38:40 -0400

    added nameList state

--- app/javascript/packs/components/Test.jsx
+++ app/javascript/packs/components/Test.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Test = () => {
-  const [user, setUser] = useState("");
+  const [name, setName] = useState("");
+  const [nameList, setNameList] = useState([]);
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("/users.json", { name }).then((response) => {
+      setName(response.data.name);
+    });
+  };
+
   useEffect(() => {
-    fetch("http://localhost:3000/users.json")
-      .then((response) => response.json())
-      .then((data) => setUser(data[0].name));
+    axios.get("/users.json").then((response) => {
+      const names = response.data;
+      console.log(names);
+      const people = [];
+      const nameItems = names.map((x) => x.name);
+      console.log(nameItems);
+
+      nameItems.map((x) => setNameList((nameList) => [...nameList, x]));
+      setNameList(nameItems);
+    });
   }, []);
+  console.log(nameList);
 
   return (
     <div>
-      <h1 className="p-4">Testing 1,2,3...{user}</h1>
+      <h1 className="p-4">Testing 1,2,3...{name}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <p>{nameList}</p>
     </div>
   );
 };
